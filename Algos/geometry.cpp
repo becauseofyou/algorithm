df5afc14b63bd2e915557ddb6cbd24cfbc91dca0
wuyiqi <593572013@qq.com>
2013-12-19 14:38:45 +0800

    basic geometry

--- Algos/geometry.cpp
+++ Algos/geometry.cpp
@@ -1,5 +1,9 @@
+
 struct point {
 	double x,y;
+	void in() {
+		scanf("%lf%lf",&x,&y);
+	}
 	point (double x=0,double y=0):x(x),y(y){}
 };
 typedef point Vector;
@@ -9,8 +13,8 @@ inline Vector operator + (const Vector &a, const Vector &b) {
 inline Vector operator - (const Vector &a, const Vector &b) {
 	return Vector(a.x - b.x, a.y - b.y);
 }
-inline Vector operator * (const Vector &a, const Vector &b) {
-	return Vector(a.x & b.x, a.y * b.y);
+inline Vector operator * (const Vector &a, double t) {
+	return Vector(a.x * t, a.y * t);
 }
 inline Vector operator / (const Vector &a, double p) {
 	return Vector(a.x / p, a.y / p);
@@ -23,4 +27,20 @@ bool operator < (const point &a, const point &b) {
 }
 bool operator == (const point &a, const point &b) {
 	return sgn(a.x - b.x) == 0 && sgn(a.y - b.y) == 0;
+}
+inline double cross(Vector a, Vector b) {
+	return a.x * b.y - a.y * b.x;
+}
+inline double dot(Vector a, Vector b) {
+	return a.x * b.x + a.y * b.y;
+}
+bool intersect(point P, Vector v, point Q, Vector w, point &p) {
+	Vector u = P - Q;
+	if(sgn(cross(v, w)) == 0) return false;
+	double t = cross(w, u) / cross(v, w);
+	p = P + v * t;
+	return true;
+}
+bool dotOnseg(point p,seg L)  {
+	return sgn(cross(L.s - p, L.e - p)) == 0 && sgn(dot(L.s - p, L.e - p)) <= 0;
 }
