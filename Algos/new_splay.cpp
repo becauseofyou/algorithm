2f906d960ff8b82c170afc272bba08d6c49d25b3
wuyiqi <593572013@qq.com>
2014-03-31 22:39:44 +0800

    Update new_splay.cpp

--- Algos/new_splay.cpp
+++ Algos/new_splay.cpp
@@ -10,162 +10,161 @@ Node* null;
 int K;
 
 struct Node {
-  Node *c[2], *fa;
-  int isRoot, sz, cnt, val;
-  int fill;
-  void setc(int d,Node *s) {
-    c[d] = s;
-    s->fa = this;
-  }
-  bool d() {
-    return fa->c[1] == this;
-  }
-  void up() {
-    sz = c[0]->sz + c[1]->sz + cnt;
-    fill = c[0]->fill + c[1]->fill + (cnt == K);
-  }
-  void clear() {
-    sz = 1;
-    isRoot = fill = 0;
-    c[0] = c[1] = fa = null;
-  }
-}pool[N];
+    Node *c[2], *p;
+    Node() {
+        c[0] = c[1] = p = null;
+        isRoot = sz = cnt = val = 0;
+    }
+    Node(int _isRoot, int _sz, int _cnt, int _val) {
+        c[0] = c[1] = p = null;
+        isRoot = _isRoot;
+        sz = _sz;
+        cnt = _cnt;
+        val = _val;
+    }
+    int isRoot, sz, cnt, val;
+    int fill;
+    inline void setc(int d,Node *s) {
+        c[d] = s;
+        s->p = this;
+    }
+    inline bool d() {
+        return p->c[1] == this;
+    }
+    void up() {
+        sz = c[0]->sz + c[1]->sz + cnt;
+        fill = c[0]->fill + c[1]->fill + (cnt == K);
+    }
+    void clear() {
+        sz = 1;
+        isRoot = fill = 0;
+        c[0] = c[1] = p = null;
+    }
+}pool[N], *C ;
 
-int top;
 Node* root[N];
 
-Node* newnode() {
-  Node *tmp;
-  tmp = &pool[top++];
-  tmp->clear();
-  return tmp;
-}
-
-void rot(Node *x,int f) {
-  Node *y = x->fa;
-  std::swap(x->isRoot, y->isRoot);
-  y->setc(!f, x->c[f]);
-  x->fa = y->fa;  
-  if(y->fa != null) y->fa->c[y->d()] = x;
-  x->setc(f, y);
-  y->up();
+/*
+ * 从高到低依次修改
+ */
+void rot(Node *x) {
+    Node *p = x->p;
+    int f = x->d();
+    std::swap(x->isRoot, p->isRoot);
+    p->p->setc(p->d(), x);
+    p->setc(f, x->c[!f]);
+    x->setc(!f, p);
+    p->up();
 }
 
 void splay(Node *x) {
-  while(!x->isRoot) {
-    if(x->fa->isRoot) {
-      rot(x, !x->d());
-    } else {
-      bool f = x->fa->d();
-      x->d() == f ? rot(x->fa, !f): rot(x, f);
-      rot(x, !f);
+    while(!x->isRoot) {
+        if(x->p->isRoot) {
+            rot(x);
+        } else {
+            bool f = x->p->d();
+            x->d() == f ? rot(x->p): rot(x);
+            rot(x);
+        }
     }
-  }
-  x->up();
+    x->up();
 }
 
-Node* insert(Node *&x, Node *y, Node *fa) {
-  if(x == null) {
-    x = y;
-    x->fa = fa;
-    return x;
-  }
-  Node *ret;
-  if(x->val == y->val) {
-    x->cnt += y->cnt;
-    ret = x;
-  }else if(x->val > y->val) {
-    ret = insert(x->c[0], y, x);
-  } else{
-    ret = insert(x->c[1], y, x);
-  }
-  x->up();
-  return ret;
+Node* insert(Node *x, Node *y) { //将y节点插入某棵splay树
+    if(x->val == y->val) {
+        x->cnt += y->cnt;
+        return x;
+    }
+    int d = y->val > x->val;
+    if(x->c[d] == null) {
+        x->setc(d, y);
+        return y;
+    }
+    return insert(x->c[d], y);
 }
-
 void debug(Node *x) {
-  if(x->c[0] != null) debug(x->c[0]);
-  printf("col=%d %d %d\n",x,x->val,x->cnt);
-  if(x->c[1] != null) debug(x->c[1]);
+    if(x->c[0] != null) debug(x->c[0]);
+    printf("col=%d %d %d\n",x,x->val,x->cnt);
+    if(x->c[1] != null) debug(x->c[1]);
 }
 
 void merge(Node *&A, Node *B) { // merge tree_B to  tree_A
-  if(A->sz <= B->sz) {
-    std::swap(A, B);
-  }
-  std::queue<Node*> Q;
-  Q.push(B);
-  while(!Q.empty()) {
-    Node *fr = Q.front(); Q.pop();
-    if(fr->c[0] != null) Q.push(fr->c[0]);
-    if(fr->c[1] != null) Q.push(fr->c[1]);
-    fr->clear();
-    Node *tmp = insert(A, fr, null);
-    splay(tmp);
-    A = tmp;
-  }
+    if(A->sz <= B->sz) {
+        std::swap(A, B);
+    }
+    std::queue<Node*> Q;
+    Q.push(B);
+    while(!Q.empty()) {
+        Node *fr = Q.front(); Q.pop();
+        if(fr->c[0] != null) Q.push(fr->c[0]);
+        if(fr->c[1] != null) Q.push(fr->c[1]);
+        fr->clear();
+        Node *tmp = insert(A, fr);
+        splay(tmp);
+        A = tmp;
+    }
 }
 
-int firstEdge[N], nextEdge[N*2], pointTo[N*2];
-int totEdge, ans[N], col[N], n;
 
-void addEdge(int a,int b) {
-  pointTo[totEdge] = b;
-  nextEdge[totEdge] = firstEdge[a];
-  firstEdge[a] = totEdge ++;
+struct Edge {
+    int v;
+    Edge *next;
+}ee[N*2], *E;
+Edge *list[N];
+void addEdge(int a, int b) {
+    Edge *e = new(E++)Edge();
+    e->v = b;
+    e->next = list[a];
+    list[a] = e;
 }
+int ans[N], col[N], n;
 
 void dfs(int u,int f) {
-  for(int iter = firstEdge[u]; iter != -1; iter = nextEdge[iter]) {
-    int v = pointTo[iter];
-    if(v == f) continue;
-    dfs(v, u);
-    merge(root[u], root[v]);
-  }
-  ans[u] = root[u]->fill;
+    for(Edge *e = list[u]; e; e = e->next) {
+        int v = e->v;
+        if(v == f) continue;
+        dfs(v, u);
+        merge(root[u], root[v]);
+    }
+    ans[u] = root[u]->fill;
 }
 
 void init() {
-  top = 0; 
-  null = new Node();
-  null->sz = 0; null->c[0] = null->c[1] = NULL; null->cnt = 0;
-  null->isRoot = 0;
-  totEdge = 0;
-  std::fill(firstEdge, firstEdge + n, -1);
-  for(int i = 0; i < n; i++) {
-    scanf("%d",&col[i]);
-  }
-  for(int i = 0; i < n; i++) { 
-    root[i] = newnode();
-    root[i]->cnt = 1;
-    root[i]->val = col[i];
-    root[i]->isRoot = 1;
-    root[i]->up();
-  }
+    C = pool; E = ee;
+    null = new (C++)Node();
+    for(int i = 0; i < n; i++) {
+        list[i] = NULL;
+        scanf("%d",&col[i]);
+    }
+    for(int i = 0; i < n; i++) { 
+        root[i] = new (C++) Node(1, 1, 1, col[i]);
+        root[i]->up();
+        //        debug(root[i]);
+    }
 }
 
 int main() {
-  int t, ca = 1;
-  scanf("%d",&t);
-  while(t--) {
-    scanf("%d%d",&n,&K);
-    init();
-    for(int i = 0, a, b; i < n - 1; i++) {
-      scanf("%d%d",&a,&b);
-      a --; b --;
-      addEdge(a, b);
-      addEdge(b, a);
+    int t, ca = 1;
+    scanf("%d",&t);
+    while(t--) {
+        scanf("%d%d",&n,&K);
+        init();
+        for(int i = 0, a, b; i < n - 1; i++) {
+            scanf("%d%d",&a,&b);
+            a --; b --;
+            addEdge(a, b);
+            addEdge(b, a);
+        }
+        dfs(0, -1);
+        int q, a;
+        if(ca > 1) puts("");
+        printf("Case #%d:\n",ca++);
+        scanf("%d",&q);
+        while(q--) {
+            scanf("%d",&a);
+            a--;
+            printf("%d\n",ans[a]);
+        }
     }
-    dfs(0, -1);
-    int q, a;
-    if(ca > 1) puts("");
-    printf("Case #%d:\n",ca++);
-    scanf("%d",&q);
-    while(q--) {
-      scanf("%d",&a);
-      a--;
-      printf("%d\n",ans[a]);
-    }
-  }
-  return 0;
+    return 0;
 }
