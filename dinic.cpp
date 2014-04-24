fb18745b672a4e81bffffb67277ce2c96cef7fb2
wuyiqi <wuyiqi.splay@gmail.com>
2014-04-24 18:35:34 +0800

    max_flow

--- dinic.cpp
+++ dinic.cpp
@@ -1,95 +1,85 @@
-#include <cstdio>
-#include <algorithm>
-
+/*
+ * 结构体或类名：Max_Flow
+ * 变量名: maxFlow;
+ * 函数名 : max_flow()
+ *
+ */
+const int N = 1100010;
+const int M = 6100000;
 const int INF = ~0u >> 2;
-const int MAX_N = 20010;
-const int MAX_E = 500010;
-
-int head[MAX_N], level[MAX_N], cur[MAX_N], pre[MAX_N];
-int nxt[MAX_E], pnt[MAX_E], cap[MAX_E], E;
-int n;
-
-void add_edge(int from,int to,int c) {
-  pnt[E] = to;
-  cap[E] = c;
-  nxt[E] = head[from];
-  head[from] = E++;
-}
-
-int Q[MAX_N], sign;
-bool bfs(int s,int t){
-  sign = t;
-  std::fill(level, level + n, -1);
-  int fr = 0, ed = 0;
-  Q[ed++] = s; level[s] = 0;
-  while(fr < ed && level[t] == -1) {
-    int u = Q[fr++];
-    for(int i = head[u]; i != -1; i = nxt[i]) {
-      if(cap[i] > 0 && level[pnt[i]] < 0) {
-        level[pnt[i]] = level[u] + 1;
-        Q[ed++] = pnt[i];
-      }
+template<class T>
+struct Max_Flow  {
+    int head[N], level[N], cur[N], pre[N];
+    int nxt[M], pnt[M], E;
+    T cap[M];
+    void init(int n, int s, int t) {
+        this->n = n;
+        this->s = s;
+        this->t = t;
+        E = 0;
+        fill(head, head + n, -1);
     }
-  }
-  return level[t] != -1;
-}
-
-void push(int t,int &flow) {
-  int mi = INF, p = pre[t];
-  for(int p = pre[t]; p != -1; p = pre[pnt[p^1]]) {
-    mi = std::min(mi,cap[p]);
-  }
-  for(int p = pre[t]; p != -1; p = pre[pnt[p^1]]) {
-    cap[p] -= mi;
-    if(!cap[p]) sign = pnt[p^1];
-    cap[p^1] += mi;
-  }
-  flow += mi;
-}
-
-void dfs(int u,int t, int &flow) {
-  if(u == t) {
-    push(t,flow);
-    return ;
-  }
-  for(int &i = cur[u]; i != -1; i = nxt[i]) {
-    if(cap[i] > 0 && level[u] < level[pnt[i]]) {
-      pre[pnt[i]] = i;
-      dfs(pnt[i],t,flow);
-      if(level[sign] < level[u]) return ;
-      sign = t;
+    void add_edge(int from, int to, T c) {
+        pnt[E] = to;
+        cap[E] = c;
+        nxt[E] = head[from];
+        head[from] = E++;
     }
-  }
-}
-
-int dinic(int s,int t) {
-  pre[s] = -1;
-  int flow = 0;
-  while(bfs(s,t)) {
-    std::copy(head,head+n,cur);
-    dfs(s,t,flow);
-  }
-  return flow;
-}
-
-int main() {
-  int m,a,b,w;
-  while(scanf("%d%d",&n,&m)!=EOF) {
-    int s = 0, t = n+1;
-    std::fill(head,head+t+1,-1);
-    E = 0;
-    for(int i = 1; i <= n; i++) {
-      scanf("%d%d",&a,&b);
-      add_edge(s,i,a); add_edge(i,s,a);
-      add_edge(i,t,b); add_edge(t,i,b);
+    bool bfs(int s, int t) {
+        sign = t;
+        fill(level, level + n, -1);
+        int *front = Q, *tail = Q;
+        *tail++ = s; level[s] = 0;
+        while(front < tail && level[t] == -1) {
+            int u = *front++;
+            for(int e = head[u]; e != -1; e = nxt[e]) {
+                if(cap[e] > 0 && level[pnt[e]] < 0) {
+                    level[pnt[e]] = level[u] + 1;
+                    *tail ++ = pnt[e];
+                }
+            }
+        }
+        return level[t] != -1;
     }
-    for(int i = 0; i < m; i++) {
-      scanf("%d%d%d",&a,&b,&w);
-      add_edge(a,b,w); 
-      add_edge(b,a,w);
+    void push(int t, int &flow) {
+        int mi = INF, p = pre[t];
+        for(int p = pre[t]; p != -1; p = pre[pnt[p ^ 1]]) {
+            mi = min(mi, cap[p]);
+        }
+        for(int p = pre[t]; p != -1; p = pre[pnt[p ^ 1]]) {
+            cap[p] -= mi;
+            if(!cap[p]) {
+                sign = pnt[p ^ 1];
+            }
+            cap[p ^ 1] += mi;
+        }
+        flow += mi;
     }
-    n = t + 1;
-    printf("%d\n",dinic(s,t));
-  }
-  return 0;
-}
+    void dfs(int u, int t, int &flow) {
+        if(u == t) {
+            push(t, flow);
+            return ;
+        }
+        for(int &e = cur[u]; e != -1; e = nxt[e]) {
+            if(cap[e] > 0 && level[u] < level[pnt[e]]) {
+                pre[pnt[e]] = e;
+                dfs(pnt[e], t, flow);
+                if(level[sign] < level[u]) {
+                    return ;
+                }
+                sign = t;
+            }
+        }
+    }
+    int dinic() {
+        pre[s] = -1;
+        int flow = 0;
+        while(bfs(s, t)) {
+            copy(head, head + n, cur);
+            dfs(s, t, flow);
+        }
+        return flow;
+    }
+    int s, t, n;
+    int Q[N], sign;
+};
