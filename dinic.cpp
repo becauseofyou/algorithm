1e8bec01e489a0aee967945f14f3c488122c0b85
wuyiqi <593572013@qq.com>
2014-06-17 12:13:07 +0800

    Update dinic.cpp

--- dinic.cpp
+++ dinic.cpp
@@ -1,14 +1,12 @@
 /*
  * 结构体或类名：Max_Flow
  * 变量名: maxFlow;
- * 函数名 : max_flow()
+ * 函数名 : Max_flow()
  *
  */
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
-using std::fill;
-using std::copy;
-using std::min;
 const int N = 20010;
 const int M = 500010;
 const int INF = ~0u >> 2;
@@ -19,22 +17,27 @@ struct Max_Flow {
     int head[N], level[N], cur[N], pre[N];
     int nxt[M], pnt[M], E;
     T cap[M];
-    void init(int n, int s, int t) {
+    void Init(int n, int s, int t) {
         this->n = n;
         this->s = s;
         this->t = t;
         E = 0;
-        fill(head, head + n, -1);
+        std::fill(head, head + n, -1);
     }
-    void add_edge(int from, int to, T c) {
+    void Add_edge(int from, int to, T c) {
         pnt[E] = to;
         cap[E] = c;
         nxt[E] = head[from];
         head[from] = E++;
+
+        pnt[E] = from;
+        cap[E] = 0;
+        nxt[E] = head[to];
+        head[to] = E++;
     }
-    bool bfs(int s, int t) {
+    bool Bfs(int s, int t) {
         sign = t;
-        fill(level, level + n, -1);
+        std::fill(level, level + n, -1);
         int *front = Q, *tail = Q;
         *tail++ = t; level[t] = 0;
         while(front < tail && level[s] == -1) {
@@ -48,11 +51,11 @@ struct Max_Flow {
         }
         return level[s] != -1;
     }
-    void push(int t, T &flow) {
+    void Push(int t, T &flow) {
         T mi = INF;
         int p = pre[t];
         for(int p = pre[t]; p != -1; p = pre[pnt[p ^ 1]]) {
-            mi = min(mi, cap[p]);
+            mi = std::min(mi, cap[p]);
         }
         for(int p = pre[t]; p != -1; p = pre[pnt[p ^ 1]]) {
             cap[p] -= mi;
@@ -63,15 +66,15 @@ struct Max_Flow {
         }
         flow += mi;
     }
-    void dfs(int u, int t, T &flow) {
+    void Dfs(int u, int t, T &flow) {
         if(u == t) {
-            push(t, flow);
+            Push(t, flow);
             return ;
         }
         for(int &e = cur[u]; e != -1; e = nxt[e]) {
             if(cap[e] > 0 && level[u] - 1 == level[pnt[e]]) {
                 pre[pnt[e]] = e;
-                dfs(pnt[e], t, flow);
+                Dfs(pnt[e], t, flow);
                 if(level[sign] > level[u]) {
                     return ;
                 }
@@ -79,36 +82,13 @@ struct Max_Flow {
             }
         }
     }
-    T dinic() {
+    T Dinic() {
         pre[s] = -1;
         T flow = 0;
-        while(bfs(s, t)) {
-            copy(head, head + n, cur);
-            dfs(s, t, flow);
+        while(Bfs(s, t)) {
+            std::copy(head, head + n, cur);
+            Dfs(s, t, flow);
         }
         return flow;
     }
 };
-
-int main() {
-    int m, a, b, w, n;
-    while(scanf("%d%d",&n,&m) != EOF) {
-        Max_Flow<int> *gao = new Max_Flow<int>();
-        gao->init(n + 2, 0, n + 1);
-        int s = 0, t = n + 1;
-        for(int i = 1; i <= n; i++) {
-            scanf("%d%d", &a, &b);
-            gao->add_edge(s, i, a);
-            gao->add_edge(i, s, a);
-            gao->add_edge(i, t, b);
-            gao->add_edge(t, i, b);
-        }
-        for(int i = 0; i < m; i++) {
-            scanf("%d%d%d",&a,&b,&w);
-            gao->add_edge(a, b, w);
-            gao->add_edge(b, a, w);
-        }
-        printf("%d\n",gao->dinic());
-    }
-    return 0;
-}
