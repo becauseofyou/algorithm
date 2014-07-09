7fb92398b3f01c24629c10ba5e69b8eec0e65b61
wuyiqi <593572013@qq.com>
2014-07-09 10:49:51 +0800

    Create kmp.cpp

---
+++ kmp.cpp
@@ -0,0 +1,33 @@
+inline void make(char *buf,int *fal)
+{
+    static int i,j;
+    fal[0]=-1;
+    for(i=1,j=-1;buf[i];++i)
+    {
+        while(j>=0 && buf[j+1]!=buf[i])
+            j=fal[j];
+        if(buf[j+1]==buf[i])
+            ++j;
+        fal[i]=j;
+    }
+}
+
+inline int match(char *p,char *t,int* fal)
+{
+    static int i,j,re;
+    for(re=i=0,j=-1;t[i];++i)
+    {
+        while(j>=0 && p[j+1]!=t[i])
+            j=fal[j];
+        if(p[j+1]==t[i])
+            ++j;
+        if(!p[j+1])
+        {
+            ++re;
+            j=fal[j];
+        }
+    }
+    return re;
+}
+const int N = 210;
+int fail[N];
