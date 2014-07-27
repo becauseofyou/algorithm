f02b9b790999ce6740b07db83f0721e4120a951d
wuyiqi <593572013@qq.com>
2014-07-27 23:27:31 +0800

    Update geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -280,20 +280,22 @@ bool Circumcenter(Point& u, Point& v, Point& w, Point& p) {			// 外接圆心
 
 /***** 圆 ******************************************************************************/
 
-double cir_area_inst(Point c1, double r1, Point c2, double r2) {			// 两圆面积交
-	double a1, a2, d, ret;
-	d = sqrt((c1.x-c2.x)*(c1.x-c2.x)+(c1.y-c2.y)*(c1.y-c2.y));
-	if ( d > r1 + r2 - eps ) 
-		return 0;
-	if ( d < r2 - r1 + eps ) 
-		return pi*r1*r1;
-	if ( d < r1 - r2 + eps )
-		return pi*r2*r2;
-	a1 = acos((r1*r1+d*d-r2*r2)/2/r1/d);
-	a2 = acos((r2*r2+d*d-r1*r1)/2/r2/d);
-	ret = (a1-0.5*sin(2*a1))*r1*r1 + (a2-0.5*sin(2*a2))*r2*r2;
-	return ret;
+double cir_area_inst(Point c1, double r1, Point c2, double r2)
+{
+        double a1, a2, d, ret;
+        d = sqrt((c1 - c2).dot(c1 - c2) );
+        if(sgn(d - r1 - r2) >= 0) 
+                return 0;
+        if(sgn(d - r2 + r1) <= 0)
+                return pi * r1 * r1;
+        if(sgn(d - r1 + r2) <= 0)
+                return pi * r2 * r2;
+        a1 = acos((r1 * r1 + d * d - r2 * r2) / 2 / r1 / d);
+        a2 = acos((r2 * r2 + d * d - r1 * r1) / 2 / r2 / d);
+        ret = (a1 - 0.5 * sin(2 * a1)) * r1 * r1 + (a2 - 0.5 * sin(2 * a2)) * r2 * r2;
+        return ret;
 }
+
 struct Ball { double x, y, z, r; };
 double Dis(Ball p, Ball q) {												// 两球体积交
 	return sqrt((p.x-q.x)*(p.x-q.x) + (p.y-q.y)*(p.y-q.y) + (p.z-q.z)*(p.z-q.z));
