6d70da9de467d7b7b87afc80ebe785c689d5238e
wuyiqi <593572013@qq.com>
2014-08-20 23:54:31 +0800

    Update Driected_Mst.cpp

--- Driected_Mst.cpp
+++ Driected_Mst.cpp
@@ -1,61 +1,75 @@
 #include <cstdio>
-#include <cstring>
 #include <algorithm>
-using namespace std;
 typedef int item;
-
-#define rep(i,n) for(int i=0;i<n;i++)
 const int inf = ~0u>>2;
-const int M = 100010;
-const int N = 100010;
-struct EDGE {
-   int u,v;
-   item cost;
-}E[M];
-int pre[N] , ID[M] , f[N];
+const int N = 1010;
+const int M = 3010;
+struct Edge
+{
+        int u, v;
+        int cost;
+}edge[M];
+int pre[N], id[M], f[N], e;
 item in[N];
-item Directed_Mst(int root,int NV,int NE) {
-    item ret = 0; int u , v;
-    while(true) {
-        fill(in,in+NV,inf);
-        rep(i,NE) {
-            u = E[i].u; v = E[i].v;
-            if(E[i].cost < in[v] && u != v) {
-                pre[v] = u;
-                in[v] = E[i].cost;
-            }
-        }	
-        rep(i,NV) {
-            if(i == root) continue;
-            if(in[i] == inf) return -1;
+void add_edge(int a, int b, int cost)
+{
+        edge[e].u = a; edge[e].v = b; edge[e].cost = cost;
+        e++;
+}
+item directed_mst(int root, int n, int m)
+{
+        item ret = 0;
+        int u, v;
+        while(1) {
+                std::fill(in, in + n, inf);
+                for(int i = 0; i < m; i++) {
+                        u = edge[i].u, v = edge[i].v;
+                        if(edge[i].cost < in[v] && u != v) {
+                                pre[v] = u;
+                                in[v] = edge[i].cost;
+                        }
+                }
+                for(int i = 0; i < n; i++) {
+                        if(i != root && in[i] == inf) {
+                                return -1;
+                        }
+                }
+                int cirs = 0;
+                std::fill(id, id + n, -1);
+                std::fill(f, f + n, -1);
+                in[root] = 0;
+                for(int i = 0; i < n; i++) {
+                        ret += in[i];
+                        int v = i;
+                        while(f[v] != i && id[v] == -1 && v != root) {
+                                f[v] = i;
+                                v = pre[v];
+                        }
+                        if(v != root && id[v] == -1) {
+                                for(int u = pre[v]; u != v; u = pre[u]) {
+                                        id[u] = cirs;
+                                }
+                                id[v] = cirs++;
+                        }
+                }
+                if(cirs == 0) {
+                        break;
+                }
+                for(int i = 0; i < n; i++) {
+                        if(id[i] == -1) {
+                                id[i] = cirs++;
+                        }
+                }
+                for(int i = 0; i < m; i++) {
+                        v = edge[i].v;
+                        edge[i].u = id[edge[i].u];
+                        edge[i].v = id[edge[i].v];
+                        if(edge[i].u != edge[i].v) {
+                                edge[i].cost -= in[v];
+                        }
+                }
+                n = cirs;
+                root = id[root];
         }
-        int circle = 0;
-        rep(i,NV) ID[i] = f[i] = -1;
-        in[root] = 0;
-        rep(i,NV) {
-            ret += in[i];
-            int v = i;
-            while(f[v] != i && ID[v] == -1 && v != root) {
-                f[v] = i;
-                v = pre[v];
-            }
-            if(v != root && ID[v] == -1) {
-                for(int u = pre[v]; u != v; u = pre[u]) ID[u] = circle;
-                ID[v] = circle ++;
-            }
-        }
-        if(circle == 0) break;
-        rep(i,NV) if(ID[i] == -1) ID[i] = circle ++;
-        rep(i,NE) {
-            v = E[i].v;
-            E[i].u = ID[E[i].u];
-            E[i].v = ID[E[i].v];
-            if(E[i].u != E[i].v) {
-                E[i].cost -= in[v];
-            }
-        }
-        NV = circle;
-        root = ID[root];
-    }
-	return ret;
+        return ret;
 }
