8b55d07d90b9cdb08919902204de6cfdc23c8241
wuyiqi <593572013@qq.com>
2014-10-04 01:25:02 +0800

    Update java.java

--- java.java
+++ java.java
@@ -1,62 +1,70 @@
 import java.util.*;
 import java.math.*;
 import java.io.*;
-public class Main {
-    public static void main(String[] args) {
-        InputStream inputStream = System.in;
-        OutputStream outputStream = System.out;
-        InputReader in = new InputReader(inputStream);
-        PrintWriter out = new PrintWriter(outputStream);
-        AC solver = new AC();
-        solver.solve(in, out);
-        out.close();
-    }
+public class Main{
+        public static void main(String[] args) {
+
+                //InputStream inputStream = System.in;
+                //OutputStream outputStream = System.out;
+                //文件操作需要抛出异常
+                try {
+                        InputStream inputStream = new FileInputStream("discuss.in");
+                        OutputStream outputStream = new FileOutputStream("discuss.out");
+                        InputReader in = new InputReader(inputStream);
+                        PrintWriter out = new PrintWriter(outputStream);
+                        AC solver = new AC();
+                        solver.solve(in, out);
+                        out.close();
+                }catch(Exception e) {
+                }
+        }
 }
 
 class AC {
-	InputReader in;
-	PrintWriter out;
+        InputReader in;
+        PrintWriter out;
         public void solve(InputReader in, PrintWriter out) {
-         this.in = in; this.out = out;
-         //ubuntu 下以ctrl + d终止 while(in.hasNext())
-    }
+                this.in = in; this.out = out;
+        }
 }
 class InputReader {
-    BufferedReader reader;
-    StringTokenizer tokenizer;
-    public InputReader(InputStream stream) {
-        reader = new BufferedReader(new InputStreamReader(stream));
-        tokenizer = null;
-    }
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+        public InputReader(InputStream stream) {
+                reader = new BufferedReader(new InputStreamReader(stream));
+                tokenizer = null;
+                //ubuntu 下以ctrl + d终止 while(in.hasNext())
+        }
 
-    public String next() {
-        if (!hasNext())
-            throw new RuntimeException();
-        return tokenizer.nextToken();
-    }
+        public String next() {
+                if (!hasNext())
+                        throw new RuntimeException();
+                return tokenizer.nextToken();
+        }
 
-    boolean hasNext() {
-        while (tokenizer == null || !tokenizer.hasMoreTokens())
-            try {
-                tokenizer = new StringTokenizer(reader.readLine());
-            } catch (Exception e) {
-                return false;
-            }
-        return true;
-    }
+        boolean hasNext() {
+                while (tokenizer == null || !tokenizer.hasMoreTokens())
+                        try {
+                                tokenizer = new StringTokenizer(reader.readLine());
+                        } catch (Exception e) {
+                                return false;
+                        }
+                return true;
+        }
 
-    public int nextInt() {
-        return Integer.parseInt(next());
-    }
+        public int nextInt() {
+                return Integer.parseInt(next());
+        }
 
-    public double nextDouble() {
-        return Double.parseDouble(next());
-    }
+        public double nextDouble() {
+                return Double.parseDouble(next());
+        }
 
-    public long nextLong() {
-        return Long.parseLong(next());
-    }
-    public BigInteger nextBigInteger() {
-    	return new BigInteger(next());
-    }
+        public long nextLong() {
+                return Long.parseLong(next());
+        }
+        public BigInteger nextBigInteger() {
+                return new BigInteger(next());
+        }
 }
+
